a66fa7b1f97f49febd4e6ea7060741a2399c95eb
Lorenzo Paolo Cocchinone <lorenzo.cocchinone@gmail.com>
2020-06-27 11:45:33 +0200

    Unity Files Upload

---
+++ Assets/Script/Gui_Controller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Gui_Controller : MonoBehaviour
+{
+
+    public GameObject ballPrefabs;
+    public InputField passwordField;
+    public Text errorText;
+
+
+
+    private Dictionary<string, int> passwordsLevel = new Dictionary<string, int>();
+    private static int balls = 0;
+
+    private void Start()
+    {
+        passwordsLevel.Add("Ax56gt789Ux", 1);
+        passwordsLevel.Add("Gv78ik903Hc", 2);
+
+        passwordField.onEndEdit.AddListener(delegate { EnterPasswordLevel(); });
+
+        
+    }
+
+    public void SpawnBall()
+    {
+        if(balls < 5)
+        {
+            Instantiate(ballPrefabs);
+            balls++;
+
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("UI");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void EnterPasswordLevel()
+    {
+        string password = passwordField.text;
+
+        if (passwordsLevel.ContainsKey(password))
+        {
+            SceneManager.LoadScene(passwordsLevel[password]);
+        }
+        else
+        {
+            errorText.GetComponent<Text>().enabled = true;
+        }
+    } 
+    
+}
